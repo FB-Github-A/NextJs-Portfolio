3ddf1f5ae3056f59330f7efc8ddda6bec4d14e09
Ismat Zehra <zehraaunty123@gmail.com>
2025-02-12 23:55:42 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,16 +14,14 @@ export default function Home() {
         />
         <h1 className="text-2xl font-bold text-gray-800 mt-4">Ismat Zehra</h1>
         <p className="text-gray-600 text-center mt-2">
-          Bachelor's of Education, Karachi University (2010) <br />
+          Bachelors of Education, Karachi University (2010) <br />
           Experienced professional at Dianetics since 2011.
         </p>
       </div>
 
       <p className="text-lg text-gray-700 text-center max-w-2xl mt-6">
-        Welcome to my portfolio! I have a strong background in education and years of experience working in an international organization.
-        My expertise lies in guiding, managing, and contributing to impactful projects.
-      </p>
-
+      Welcome to my portfolio! I have a strong background in education and years of experience working in an international organization.
+      My expertise lies in guiding, managing, and contributing to impactful projects.</p>
       <div className="mt-8">
         <ProjectCard
           title="Awesome Project"
